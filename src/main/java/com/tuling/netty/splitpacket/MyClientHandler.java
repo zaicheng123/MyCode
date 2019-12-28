2107834ccfef151def1390bba3cbfc49dbe1591d
zhuge <394970971@qq.com>
2019-12-28 10:12:50 +0800

    第三期kafka课程配套代码

--- src/main/java/com/tuling/netty/splitpacket/MyClientHandler.java
+++ src/main/java/com/tuling/netty/splitpacket/MyClientHandler.java
@@ -8,7 +8,7 @@ public class MyClientHandler extends SimpleChannelInboundHandler<MyMessageProtoc
 
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
-        for(int i = 0; i< 5; i++) {
+        for(int i = 0; i< 200; i++) {
             String msg = "你好，我是张三！";
             //创建协议包对象
             MyMessageProtocol messageProtocol = new MyMessageProtocol();
@@ -16,7 +16,6 @@ public class MyClientHandler extends SimpleChannelInboundHandler<MyMessageProtoc
             messageProtocol.setContent(msg.getBytes(CharsetUtil.UTF_8));
             ctx.writeAndFlush(messageProtocol);
         }
-
     }
 
     @Override
